309922dbd3de416a47ce8945771bca59562b0660
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-12-03 20:43:09 +0300

    add template for article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+import { ArticleComponent } from './article/article.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent, TopHeaderComponent, MainComponentComponent, ArticleComponent],
+   imports: [BrowserModule, AppRoutingModule],
+   providers: [],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
