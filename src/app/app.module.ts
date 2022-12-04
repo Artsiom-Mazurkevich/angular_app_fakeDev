21f4f33a9fd889fcaa8795bf3a148c0c14d62a9a
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-12-04 23:43:52 +0300

    add color mode & base routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
-import { AppRoutingModule } from './app-routing.module'
+import { Routes, RouterModule } from '@angular/router'
 import { AppComponent } from './app.component'
-import { TopHeaderComponent } from './top-header/top-header.component';
-import { MainComponentComponent } from './main-component/main-component.component';
+import { TopHeaderComponent } from './top-header/top-header.component'
+import { MainComponent } from './main-component/main.component'
 import { ArticleComponent } from './article/article.component'
+import { SignInUpComponent } from './sign-in-up/sign-in-up.component'
+
+const appRoutes: Routes = [
+   { path: '', component: MainComponent },
+   { path: 'auth/signup', component: SignInUpComponent },
+   { path: 'auth/signin', component: SignInUpComponent },
+]
 
 @NgModule({
-   declarations: [AppComponent, TopHeaderComponent, MainComponentComponent, ArticleComponent],
-   imports: [BrowserModule, AppRoutingModule],
+   declarations: [AppComponent, TopHeaderComponent, MainComponent, ArticleComponent, SignInUpComponent],
+   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
    providers: [],
    bootstrap: [AppComponent],
 })
